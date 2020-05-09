958dc391f51986932ea6e6d06a5af2cf777760f5
Daniel R. Grayson <dan@math.uiuc.edu>
2020-05-08 19:56:18 -0500

    improvements

--- D language/foo.d
+++ D language/foo.d
@@ -1,13 +1,18 @@
 b := true;
+export c := 345;
+(x:int) + (y:int) ::= Ccode(int,"(",x," + ",y,")");
+d := 2 + 2;
 i := if b then 11 else 22;
-export X := {  x:int, y:int};
-export Y := {+ a:int, b:int, c:bool};
-export Z := {+ d:int, e:int};
-export U := Y or Z;
-export foo ( u:U ) : int := (
+X := {  x:int, y:int};
+Y := {+ a:int, b:int, c:bool};
+Z := {+ d:int, e:int};
+U := Y or Z;
+foo ( u:U ) : int := (
      when u
      is y:Y do y.a
      is z:Z do z.d
      );
-export bar () : U := Y(111,222,true);
-export car () : U := Z(333,444);
+bar () : U := Y(111,222,true);
+car () : U := Z(333,444);
+u1 := bar();
+u2 := car();
